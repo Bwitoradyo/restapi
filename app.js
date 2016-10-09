369af214564ce1d67b4727c1c128c7429e7eea37
Bambang Witoradyo <bambang.witoradyo@sonoco.com>
2016-10-09 17:07:50 -0400

    implement PUT

--- app.js
+++ app.js
@@ -14,57 +14,16 @@ var Book = require('./models/bookModel.js');
 var app = express();
 var port = process.env.PORT || 3000;
 
+//alse use urlencoded
 app.use(bodyParser.urlencoded({extended:true}))
+//use body parser middle ware
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+var bookRouter = require("./Routes/bookRoutes.js")(Book);
 
 
-bookRouter.route('/Books')
-//implement POST
-    .post(function (req, res) {
-        var book = new Book(req.body);
-
-        book.save();
-        res.status(201).send(book);
-
-    })
-//implement GET
-    .get(function (req, res) {
-        var query = {};
-        if(req.query.genre){
-            query.genre =  req.query.genre;
-        }
-       Book.find(query, function (err, books) {
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books)
-        });
-    });
-
-
-//implement GET for Book ID
-bookRouter.route('/Books/:bookId')
-    .get(function (req, res) {
-
-        Book.findById(req.params.bookId, function (err, book) {
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(book)
-        });
-    });
-
-
-
-
-
-
-
-app.use('/api', bookRouter);
-
-
+app.use('/api/books', bookRouter);
+//app.use('/api/authors', authorRouter);
 
 
 app.get('/', function(req, res){
